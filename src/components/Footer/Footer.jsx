7a982b50ba6deb15c77e83399ad4fae95a32601a
danacmsantiago <danacmsantiago@gmail.com>
2024-04-29 15:17:09 +0800

    commit-2

--- src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer>
-      &copy; {new Date().getFullYear()} goit-react-hw-08-phonebook
-    </footer>
-  );
-};
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Suspense } from 'react';
 import css from './SharedLayout.module.css';
-import { Footer } from 'components/Footer/Footer';
 
 export const SharedLayout = () => {
   return (
@@ -11,7 +10,6 @@ export const SharedLayout = () => {
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-      <Footer />
     </div>
   );
 };
